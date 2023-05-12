990f09cc94b549ca21409923f1db540083446ded
Büşra Güral <busramgural@gmail.com>
2023-05-12 13:29:55 +0300

    functions created

--- 3. Graph and Adj Matrix/graph.c
+++ 3. Graph and Adj Matrix/graph.c
@@ -2,7 +2,6 @@
 #include<stdlib.h>
 
 
-
 typedef struct node {
 	
     int dest;
@@ -112,25 +111,88 @@ void numberOfPrerequisites(Graph* graph, int n, int *arr){
 		
 		while(tmp){
 			
-			arr[tmp->dest] +=1;
+			arr[tmp->dest] ++;
 			tmp = tmp->next;
 		}
 	}
-	printf("\n");
+}
+
+void printPrerequisites(int *arr, int n){
+	
+	int i;
 	
 	for(i=0; i<n; i++){
-		printf("Course-%d: ", i+1);
-		printf("%d\t", arr[i]);
+		if(arr[i] != -1){
+			printf("Course-%d: ", i+1);
+			printf("%d\t", arr[i]);
+		}
+		
+	}
+}
+
+
+
+void semesterCount(Graph *graph, int n, int *arr, int *semester){
+	
+	int i;
+	Node *tmp;
+	int *indegree = (int*)malloc(n*sizeof(int));
+	
+	for(i=0; i<n; i++){
+		indegree[i] = arr[i];
 	}
 	
+	int j =0;
+	
+	int count;
+	
+	do{
+		
+		count = 0;	
+		j++;
+		for(i=0; i<n; i++){
+			
+			if(arr[i] == 0){
+			
+				indegree[i]= -1;
+				printf("\nSemester %d -> Course-%d is taken.\n", j, i+1);
+				tmp = graph->adjacencyLists[i];
+
+				while(tmp != NULL){
+					
+					indegree[tmp->dest]--;
+					tmp = tmp->next;
+				}
+				
+			}
+			
+		}
+		
+		for(i=0; i<n; i++){
+			arr[i]  = indegree[i];
+		}
+		
+	
+		for(i=0; i<n; i++){
+			if(indegree[i] == -1){
+				count++;
+			}
+		}
+		printf("\n");
+		printPrerequisites(arr, n);
+		
+	}while(count !=n );
+	
+	*semester = j;
 }
+
 
 
 int main(){
 	
 	int n;
 	int **adjacencyMatrix;
-	
+	int semester=0;
 	Graph* graph = (Graph*) malloc(sizeof(Graph));
 	
 	
@@ -145,9 +207,14 @@ int main(){
     printGraph(graph);
     
 	int *arr = (int*)calloc(n,sizeof(int));
-	numberOfPrerequisites(graph, n, arr);
-
 	
+	numberOfPrerequisites(graph, n, arr);
+	printPrerequisites(arr, n);
+
+
+	semesterCount(graph, n, arr, &semester);
+	printf("The student finishes school in %d semesters.", semester);
+
 	
 	return 0; 
 }
